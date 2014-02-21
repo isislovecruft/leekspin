8ec14cbd6dce832545dafe47e794faba0bffae15
Isis Lovecruft <isis@torproject.org>
2014-02-21 19:28:41 +0000

    Add __init__.py and files generated by versioneer.

---
+++ leekspin/__init__.py
@@ -0,0 +1,14 @@
+#_____________________________________________________________________________
+#
+# This file is part of LeekSpin, an Onion Router descriptor generator.
+#
+# :authors: Isis Lovecruft <isis@torproject.org>   0xA3ADB67A2CDB8B35
+#           Matthew Finkel <sysrqb@torproject.org> 0x017DD169EA793BE2
+# :copyright: (c) 2013-2014, The Tor Project, Inc.
+#             (c) 2013-2014, all entities within the AUTHORS file
+# :license: see LICENSE for licensing information
+#_____________________________________________________________________________
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
