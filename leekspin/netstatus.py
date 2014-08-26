658d1f9c165eae818fda064ac50f74a7ac33ff8c
Isis Lovecruft <isis@torproject.org>
2014-08-26 00:21:24 +0000

    Better documentation for leekspin.netstatus.generateNetstatus().

--- leekspin/netstatus.py
+++ leekspin/netstatus.py
@@ -21,18 +21,24 @@ def generateNetstatus(nickname, idkey_digest, server_desc_digest, timestamp,
                       ipv4, orport, ipv6=None, dirport=None,
                       flags='Fast Guard Running Stable Valid',
                       bandwidth_line=None):
-    """Generate an ``@type networkwork-status`` document (unsigned).
+    """Generate an ``@type bridge network-status 1.0`` document (unsigned).
 
-    DOCDOC
+    This function will generate a networkstatus document for a bridge relay,
+    similar to the following::
+
+        r StingilyScampers Wdtrb4h8QVqqbDH4gMmVnAn2nYg 1BEVkVjixzVMFu7OK46GklhYtkg 2014-08-06 20:40:21 39.102.19.106 36286 0
+        a [72be:7d50:9c91:1170:2bf9:d760:bee1:66e1]:36286
+        s Fast Guard Running Stable Valid
+        w Bandwidth=1481409
+        p reject 1-65535
 
     :param str nickname: The router's nickname.
     :param string idkey_digest: The SHA-1 digest of the router's public identity
         key.
-    :param XXX server_desc_digest: The SHA-1 digest of the router's
+    :param str server_desc_digest: The SHA-1 digest of the router's
         ``@type [bridge-]server-descriptor``, before the descriptor is signed.
-    :param XXX timestamp:
+    :param str timestamp: An ISO 8601 timestamp, with a space as the separator.
     """
-
     idkey_b64  = binascii.b2a_base64(idkey_digest)
     idb64      = str(idkey_b64).strip().rstrip('==')
     server_b64 = binascii.b2a_base64(server_desc_digest)
